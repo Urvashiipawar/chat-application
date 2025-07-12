1417d3820b641ab4649a58fe89c9ca7f43ecdeb7
Urvashiipawar <122180221+Urvashiipawar@users.noreply.github.com>
2025-07-12 22:06:43 +0200

    login and registeration page done

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b9fac58923449290347659bc629512067a4886f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+177df0c5674f90c2975bc7849c2dd4dcc66badf8")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -5,6 +5,10 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
+import { ApiResponse } from '../models/api-response';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -15,16 +19,43 @@ import { MatIconModule } from '@angular/material/icon';
 export class RegisterComponent {
   email!: string;
   password!: string;
+  userName!: string;
   fullName!: string;
   profilePicture: string = 'https://randomuser.me/api/portraits/lego//5.jpg';
   profileImage: File | null = null;
 
   authService = inject(AuthServiceService);
-  hide = signal(true);
+  snackBar = inject(MatSnackBar);
+  router= inject(Router);
+  hide = signal(false);
 
   togglePassword(event : MouseEvent){
     this.hide.set(!this.hide());
+    event.stopPropagation();
     }
+
+    register(){
+      let formData = new FormData();
+      formData.append('email', this.email);
+      formData.append('password', this.password);
+      formData.append('userName', this.userName);
+      formData.append('fullName', this.fullName);
+      formData.append('profileImage', this.profileImage!);
+
+      this.authService.register(formData).subscribe({
+        next: () => {
+          this.snackBar.open('User registered successfully', 'Close');
+
+        },
+        error:(error:HttpErrorResponse)=>{
+          let err= error.error as ApiResponse<string>;
+          this.snackBar.open(err.error, "Close")
+        },
+        complete: () => {
+          this.router.navigate(['/'])
+        },
+    });
+  }
 
   onFileSelected(event: any){
     const file:File = event.target.files[0];
